b3008eb0a3f9641d25fb54c9487fba981c6c3dad
Younger Shen <younger.x.shen@gmail.com>
2019-03-06 15:25:06 +0800

    Feature: add bootstrap

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PROJECT : wordpress-5.0.3
+ * FILE    : index.php
+ * TIME    : 2019/3/6 15:14
+ * AUTHOR  : Younger Shen
+ * EMAIL   : younger.x.shen@gmail.com
+ * PHONE   : 13811754531
+ * WECHAT  : 13811754531
+ * WEBSIT  : https://www.punkcoder.cn
+ */
+ 
+ 
