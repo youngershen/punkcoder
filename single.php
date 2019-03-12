a14e260a723347b832ed2cd4ec124b9030a363c5
Younger Shen <younger.x.shen@gmail.com>
2019-03-12 10:19:03 +0800

    Feature: add single.php

---
+++ single.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PROJECT : punkcoder
+ * FILE    : single.php
+ * TIME    : 2019/3/12 10:11
+ * AUTHOR  : Younger Shen
+ * EMAIL   : younger.x.shen@gmail.com
+ * PHONE   : 13811754531
+ * WECHAT  : 13811754531
+ * WEBSIT  : https://www.punkcoder.cn
+ */
+ 
+ 
