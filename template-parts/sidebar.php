7417de9cf83a0ba45c284dc001dc39e6d5d8e66e
Younger Shen <younger.x.shen@gmail.com>
2019-03-13 17:30:02 +0800

    Feature: add hot post logics componment of sidebar

--- template-parts/sidebar.php
+++ template-parts/sidebar.php
@@ -9,8 +9,30 @@
  * WECHAT  : 13811754531
  * WEBSIT  : https://www.punkcoder.cn
  */
+$categories = get_categories(array(
+    'orderby' => 'name',
+    'order' => 'ASC'
+));
+
+$tags = get_tags();
+
+$args = array(
+    'posts_per_page' => 10,
+    'category' => 7,
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+    'post_type' => 'post'
+);
+
+$args = array(
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+);
+
+$query = new WP_Query($args);
 
 ?>
+
 <div class="d-none d-lg-block punkcoder-sidebar container-fluid">
     <div class="row punkcoder-sidebar-profile justify-content-center">
         <div class="col-12">
@@ -53,45 +75,42 @@
         </div>
     </div>
     <hr>
-    <div class="row punkcoder-sidebar-hot">
-        <div class="col-12 punkcoder-sidebar-hot-col">
-            <div class="punkcoder-sidebar-hot-head">
-                <span>热门文章</span>
-            </div>
-            <div class="punkcoder-sidebar-hot-body">
-                <div>
-                    <a href="">士大夫胜多负少发士大夫胜多负少发士大夫胜多负少发士大夫胜多负少发</a>
-                    <hr>
+
+    <?php
+
+    if ($query->have_posts()) {
+        ?>
+        <div class="row punkcoder-sidebar-hot">
+            <div class="col-12 punkcoder-sidebar-hot-col">
+                <div class="punkcoder-sidebar-hot-head">
+                    <span>热门文章</span>
                 </div>
-                <div>
-                    <a href="">士大夫胜多负少发</a>
-                    <hr>
-                </div>
-                <div>
-                    <a href="">士大夫胜多负少发</a>
-                    <hr>
-                </div>
-                <div>
-                    <a href="">士大夫胜多负少发</a>
-                    <hr>
-                </div>
-                <div>
-                    <a href="">士大夫胜多负少发</a>
-                    <hr>
-                </div>
-                <div>
-                    <a href="">士大夫胜多负少发</a>
+                <div class="punkcoder-sidebar-hot-body">
+                    <?php
+                    global $post;
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        ?>
+                        <div>
+                            <a href="<?php echo(get_permalink($post)); ?>"><?php echo(get_the_title()); ?></a>
+                        </div>
+                        <?php
+                        if ($query->post_count != $query->current_post + 1) {
+                            ?>
+                            <hr>
+                            <?php
+                        }
+                    }
+                    ?>
                 </div>
             </div>
         </div>
-    </div>
+
+        <?php
+    }
+
+    ?>
     <?php
-
-    $categories = get_categories(array(
-        'orderby' => 'name',
-        'order' => 'ASC'
-    ));
-
     if ($categories) {
         ?>
         <hr>
@@ -115,8 +134,6 @@
                             <?php
                         }
                         ?>
-
-
                         <?php
                     }
                     ?>
@@ -128,31 +145,30 @@
         <?php
     }
     ?>
-    <div class="row punkcoder-sidebar-tag">
-        <div class="col-12">
-            <div class="punkcoder-sidebar-tag-head">
-                <span>标签</span>
-            </div>
-            <div class="punkcoder-sidebar-tag-body">
-                <a href="">
-                    <span class="badge badge-primary punkcoder-sidebar-tag-item">JAVA</span>
-                </a>
-                <a href="">
-                    <span class="badge badge-primary punkcoder-sidebar-tag-item">Linux</span>
-                </a>
-                <a href="">
-                    <span class="badge badge-primary punkcoder-sidebar-tag-item">编程</span>
-                </a>
-                <a href="">
-                    <span class="badge badge-primary punkcoder-sidebar-tag-item">教程</span>
-                </a>
-                <a href="">
-                    <span class="badge badge-primary punkcoder-sidebar-tag-item">游戏</span>
-                </a>
-                <a href="">
-                    <span class="badge badge-primary punkcoder-sidebar-tag-item">电影</span>
-                </a>
+    <?php
+    if ($tags) {
+
+        ?>
+        <div class="row punkcoder-sidebar-tag">
+            <div class="col-12">
+                <div class="punkcoder-sidebar-tag-head">
+                    <span>标签</span>
+                </div>
+                <div class="punkcoder-sidebar-tag-body">
+                    <?php
+                    foreach ($tags as $key => $val) {
+                        $tag_link = get_tag_link($val->term_id);
+                        ?>
+                        <a href="<?php echo($tag_link); ?>">
+                            <span class="badge badge-primary punkcoder-sidebar-tag-item"><?php echo($val->name) ?></span>
+                        </a>
+                        <?php
+                    }
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
+        <?php
+    }
+    ?>
 </div>
