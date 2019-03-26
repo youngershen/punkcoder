2e3d9fc12b3885cec975870bb0239f1b053b77b4
Younger Shen <younger.x.shen@gmail.com>
2019-03-26 16:41:11 +0800

    Fix : add custom background option doesn't show correct issue

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -52,12 +52,11 @@ $query = new WP_Query($args);
         <?php get_search_form(); ?>
     </div>
 </nav>
-<div id="pk-slogan" class="container-fluid d-none d-lg-block" style="background-image: url("<?php echo punkcoder_get_options('punkcoder_options', 'bg_image', punkcoder_get_url('images', 'default-bg-image.png'))?>")">
-    <div class="row justify-content-center bg-secondary slogan-bg">
+<div id="pk-slogan" class="container-fluid d-none d-lg-block">
+    <div class="row justify-content-center bg-secondary slogan-bg" style="background-image: url('<?php echo punkcoder_get_options('punkcoder_options', 'bg_image', punkcoder_get_url('images', 'default-bg-image.png'))?>') !important;">
         <div class="col-6 align-content-center my-5 text-light">
             <h1 class="text-center "><?php bloginfo('name'); ?></h1>
             <h2 class="text-center mt-5"><?php bloginfo('description'); ?></h2>
         </div>
     </div>
 </div>
-
