5eae4c42bb10b14aa7bab3a16f71709df46c5d6f
Younger Shen <younger.x.shen@gmail.com>
2019-04-24 09:44:24 +0800

    Style: change post comment form style

--- comments.php
+++ comments.php
@@ -62,26 +62,19 @@ $req = get_option( 'require_name_email' );
 $commenter = wp_get_current_commenter();
 //
 $fields   =  array(
-    'author'  => '  <div class="form-row"> <div class="form-group col-md-6">
-                     <label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
-                    '<input type="text" class="form-control" placeholder="张三" id="author" name="author" value="' . esc_attr( $commenter['comment_author'] ) . '"' . ($req ? 'required="required"' : '') . '> 
-                    </div>',
+    'author'  => '<div class="form-row"><div class="form-group col-md-6"><label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label><input type="text" class="form-control" placeholder="' . __('张三', 'punkcoder') . '" id="author" name="author" value="' . esc_attr( $commenter['comment_author'] ) . '" ' . ($req ? 'required="required"' : ''). '></div>',
 
-    'email'  => '<div class="form-group col-md-6">
-                     <label for="email">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
-                    '<input type="email" class="form-control" placeholder="zhangsan@gmail.com" id="email" name="email" value="' . esc_attr( $commenter['comment_author_email'] ) . '"' . ($req ? 'required="required"' : '') . '> 
-                    </div></div>'
+    'email'  => '<div class="form-group col-md-6"><label for="email">' . __( 'Email' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label><input type="email" class="form-control" placeholder="' . __('zhangsan@gmail.com', 'punkcoder') . '" id="email" name="email" value="' . esc_attr( $commenter['comment_author_email'] ) . '" ' . ($req ? 'required="required"' : '') .  '></div></div>'
 );
+
+$comment_field = '<div class="form-group"><label for="comment">' .__('留言', 'punkcoder').'</label><textarea class="form-control" id="comment" rows="3" name="comment"  maxlength="65525" required="required">'. __('内容不错！', 'punkcoder') .'</textarea></div>';
 
 $args = [
     'title_reply' => __('发表评论', 'punkcoder'),
     'title_reply_to' => __( '向 %s 回复评论', 'punkcoder'),
-    'comment_notes_before' => '<h5>电子邮件地址不会被公开。 必填项已用 * 标注</h5>',
+    'comment_notes_before' => '<h5>'. __('电子邮件地址不会被公开 必填项已用 * 标注', 'punkcoder') . '</h5>',
     'comment_notes_after' => '',
-    'comment_field' => '<div class="form-group">
-                            <label for="comment">留言</label>
-                            <textarea class="form-control" id="comment" rows="3" name="comment"  maxlength="65525" required="required">内容不错！</textarea>
-                        </div>',
+    'comment_field' => $comment_field,
     'fields' => $fields,
     'submit_field' => '%1$s %2$s',
     'submit_button' => '<input name="%1$s" type="submit" id="%2$s" class="%3$s btn btn-primary" value="%4$s" />',
