ae75ef59c1d342cf73d1836ff60cbf7e75375814
Younger Shen <younger.x.shen@gmail.com>
2019-05-08 17:09:33 +0800

    Fix: page title display issue

--- template-parts/post-single.php
+++ template-parts/post-single.php
@@ -12,6 +12,9 @@
 ?>
 
     <article <?php post_class('single-article justify-content-center'); ?> id="post-<?php the_ID(); ?>">
+        <header class="single-article-header justify-content-center d-lg-none">
+            <h2 class="justify-content-center single-article-title"><?php echo($post->post_title) ?></h2>
+        </header>
         <div class="single-article-meta">
             <?php get_template_part('template-parts/post-meta') ?>
         </div>
